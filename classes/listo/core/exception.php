e48f80d17821c5663ab5cac854da498c6767270e
mtou <mtou@charougna.com>
2011-11-14 13:09:02 +0100

    Add Listo_Exception

---
+++ classes/listo/core/exception.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares Listo_Core_Exception
+ *
+ * PHP version 5
+ *
+ * @group listo
+ *
+ * @category  Listo
+ * @package   Listo
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listo/tree/master/classes/listo/core/exception.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Listo_Core_Exception
+ *
+ * PHP version 5
+ *
+ * @group listo
+ *
+ * @category  Listo
+ * @package   Listo
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listo/tree/master/classes/listo/core/exception.php
+ */
+abstract class Listo_Core_Exception extends Kohana_Exception
+{
+} // End Listo_Core_Exception
---
+++ classes/listo/exception.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Declares Listo_Exception
+ *
+ * PHP version 5
+ *
+ * @group listo
+ *
+ * @category  Listo
+ * @package   Listo
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listo/tree/master/classes/listo/exception.php
+ */
+
+defined('SYSPATH') OR die('No direct access allowed.');
+
+/**
+ * Provides Listo_Exception
+ *
+ * PHP version 5
+ *
+ * @group listo
+ *
+ * @category  Listo
+ * @package   Listo
+ * @author    mtou <mtou@charougna.com>
+ * @copyright 2011 mtou
+ * @license   http://www.debian.org/misc/bsd.license BSD License (3 Clause)
+ * @link      https://github.com/emtou/kohana-listo/tree/master/classes/listo/exception.php
+ */
+class Listo_Exception extends Listo_Core_Exception
+{
+}
